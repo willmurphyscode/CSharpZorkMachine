d1ea9c4b4c72294cf53ace836c156c587d5b16e0
William Murphy <willmurphyscode@gmail.com>
2016-02-20 05:49:19 -0500

    Added addition and substraction overloads for byte address struct

--- CSharpZorkMachine/ByteAddress.cs
+++ CSharpZorkMachine/ByteAddress.cs
@@ -15,5 +15,14 @@ namespace CSharpZorkMachine
             this.value = value; 
         }
         public int Value { get { return this.value; } }
+
+        public static ByteAddress operator +(ByteAddress address, int offset)
+        {
+            return new ByteAddress(address.Value + offset);
+        }
+        public static ByteAddress operator -(ByteAddress address, int offset)
+        {
+            return address + (0 - offset);
+        }
     }
 }
