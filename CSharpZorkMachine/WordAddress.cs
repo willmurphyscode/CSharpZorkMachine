e1bceddc8be0ed25a3e3aabd722e275f5ac50292
William Murphy <willmurphyscode@gmail.com>
2016-02-25 20:47:56 -0500

    Added addition operator to WordAddress struct

--- CSharpZorkMachine/WordAddress.cs
+++ CSharpZorkMachine/WordAddress.cs
@@ -8,11 +8,17 @@ namespace CSharpZorkMachine
 {
     public struct WordAddress
     {
+        private const int WordSize = 2; 
         private int value;
         public WordAddress(int value)
         {
             this.value = value; 
         }
         public int Value { get { return this.value; } }
+
+        public static WordAddress operator +(WordAddress address, int offset)
+        {
+            return new WordAddress(address.Value + (WordSize * offset)); 
+        }
     }
 }
