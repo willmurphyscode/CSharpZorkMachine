f1784b638e11d6a9e35250458f29c5b42fb04576
William Murphy <willmurphyscode@gmail.com>
2016-02-26 06:51:31 -0500

    Added xunit to unit test project, added test word word struct

---
+++ CSharpZorkMachine.Tests/TestWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpZorkMachine;
+using Xunit;
+
+namespace CSharpZorkMachine.Tests
+{
+    public class TestWord
+    {
+        [Fact]
+        public void WordDotIsTerminalReturnsCorrectValue()
+        {
+            Word noTerminal = new Word(0);
+            Word terminal = new Word(~0);
+            Word onlyTerminalBitSet = new Word(65535 / 2 + 1);
+
+            Assert.True(terminal.IsTerminal());
+            Assert.True(onlyTerminalBitSet.IsTerminal());
+            Assert.True(!noTerminal.IsTerminal());
+        }
+    }
+}
