09d5ebb87257a0455caf80f6415795ecbad89b1d
William Murphy <willmurphyscode@gmail.com>
2016-03-05 22:11:02 -0500

    very rough skeleton of string reading function. Work in progress check-in

--- CSharpZorkMachine/AbbreviationTableStructs.cs
+++ CSharpZorkMachine/AbbreviationTableStructs.cs
@@ -102,8 +102,11 @@ namespace CSharpZorkMachine
     }
     public struct Zchar
     {
+        enum state { abbr, abbr32, abbr64, upper, symbol, lower, ascii, ascii2 };
         private int value;
-        private static char[] Table = { ' ', '?', '?', '?', '?', '?', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' }; 
+        private static char[] Lower = { ' ', '?', '?', '?', '?', '?', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        private static char[] Upper = { ' ', '?', '?', '?', '?', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        private static char[] Symbol = { ' ', '?', '?', '?', '?', '?', '?', '\n', '0', '1', '2', '3', '4', '5', '6', '7','8', '9', '.', ',', '!', '?', '_', '#', '\'', '"', '/', '\\', '-', ':', '(', ')' }; 
         public Zchar(int value)
         {
             this.value = value;
@@ -111,8 +114,77 @@ namespace CSharpZorkMachine
         public int Value { get { return this.value; } }
         public static string PrintFromZchar(IEnumerable<Zchar> chars)
         {
-            char[] values = chars.Select(ch => Table[ch.Value]).ToArray();
+            char[] values = chars.Select(ch => Lower[ch.Value]).ToArray();
             return new string(values);
+        }
+        
+        public static IEnumerable<char> DecodeFromZString(IEnumerable<Zchar> chars, GameMemory memory)
+        {
+            //THIS IS the least functional-style code in here. it's so stateful. (well, it's a state machine)
+            //another way would be to write a function that 
+            //takes a state and a zchar and decides what to do. 
+            const int lowEntry = 6;
+            Zchar? previous = null;
+            state current = state.lower;
+            foreach(Zchar ch in chars)
+            {
+                if(ch.Value >= lowEntry)
+                {
+                    switch(current)
+                    {
+                        case state.abbr:
+                        case state.abbr32:
+                        case state.abbr64:
+                            throw new NotImplementedException(); 
+                            //TODO insert an entire string dereferenced from the abbreviation table here,
+                            // probably with some horrible, nested state-machine-for-loop
+                            break; 
+                        
+                        case state.lower:
+                            yield return Lower[ch.Value];
+                            break;
+                        case state.upper:
+                            current = state.lower;
+                            yield return Upper[ch.Value];
+                            break;
+                        case state.symbol:
+                            if(ch.Value == 6)
+                            {
+                                current = state.ascii;
+                                continue;
+                            }
+                            else
+                            {
+                                current = state.lower;
+                                yield return Symbol[ch.Value]; 
+                            }
+                            break;
+                        case state.ascii:
+                            previous = ch;
+                            current = state.ascii2;
+                            continue;
+                        case state.ascii2:
+                            throw new System.NotImplementedException();
+                            //TODO the previous char and the currnt char
+                            // are combined to make an ascii char
+                            // not in the tables. 
+                            break; 
+                    }
+                }
+                else
+                {
+                    switch(ch.Value)
+                    {
+                        case 4:
+                            current = state.upper;
+                            break;
+                        case 5:
+                            current = state.abbr;
+                            break;
+                    }
+                    continue;
+                }
+            }
         }
         public static string PrintWordAsZchars(Word word)
         {
@@ -128,7 +200,7 @@ namespace CSharpZorkMachine
             StringBuilder retval = new StringBuilder();
             foreach(Zchar ch in chars)
             {
-                retval.Append($"{ch.Value.ToString("X")} {Table[ch.Value]} |");
+                retval.Append($"{ch.Value.ToString("X")} {Lower[ch.Value]} |");
             }
             return retval.ToString();
         }
